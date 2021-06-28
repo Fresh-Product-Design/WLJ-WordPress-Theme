c81e98e260a2a07f1ba997c29a450c4ab83179dc
Zach Case <zach@sherpadigitalmedia.com>
2021-06-28 10:29:27 -0600

    Continue work on menus/navigation/header

--- template-parts/header/site-header.php
+++ template-parts/header/site-header.php
@@ -1,5 +1,5 @@
 <header class="w-full bg-white py-4 shadow-md" role="banner">
-  <div class="container mx-auto">
+  <div class="container mx-auto flex items-center justify-between">
     <?php get_template_part( 'template-parts/header/site-branding' ); ?>
     <?php get_template_part( 'template-parts/header/site-nav' ); ?>
   </div>
