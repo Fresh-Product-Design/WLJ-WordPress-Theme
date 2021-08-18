c8687ae7bf7a79d548f316e9fbf2adfc96b40bd4
Zach Case <zach@sherpadigitalmedia.com>
2021-08-18 16:05:23 -0600

    Add-on Work Speaking Services

--- template-parts/pages/services/speaking/speaking-topics.php
+++ template-parts/pages/services/speaking/speaking-topics.php
@@ -9,31 +9,35 @@
   <div class="bg-white p-8 space-y-8 rounded">
     <p class="font-bold text-2xl">Video Examples</p>
     <!-- Disrupt Yourself -->
-    <div class="flex flex-col md:flex-row border-b border-[#BFBFBF] pb-8 lg:border-none lg:pb-0">
+    <div class="flex flex-col md:flex-row border-b border-[#d4d4d4] pb-8">
       <div class="mb-8 md:mb-0 md:mr-8 flex-shrink-0">
         <div class="relative">
           <img class="w-full md:w-[320px] lg:w-[500px]" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/services/video-disrupt-yourself.png') ?>" alt="" width="500" height="274" />
         </div>
       </div>
-      <div class="space-y-4">
-        <h4 class="text-xl font-bold">Disrupt Yourself</h4>
-        <p class="text-[#4d4d4d]">We are living in an era of accelerating disruption- managing and mastering the S Curve is a requisite skill. If you want to be successful in unexpected ways, follow your own disruptive path. Dare to innovate. Do something astonishing. Disrupt yourself.</p>
-        <a class="group flex items-center font-bold text-[#017381] hover:text-[#143b3f] cursor-pointer" data-js="fireContactModal">
-          <span class="mr-1 group-hover:underline">Inquire Now</span>
-          <span class="material-icons-round mr">arrow_forward</span>
-        </a>
+      <div>
+        <h4 class="text-xl font-bold">Disrupt or Be Disrupted</h4>
+        <h5 class="text-lg mb-4">Growing your Business and Yourself in An Era of Disruptive Change</h5>
+        <div class="space-y-4">
+          <p class="text-[#4d4d4d]">Consider this simple yet powerful idea: disruptive companies and ideas upend markets by doing something truly different. They see a need, an empty space waiting to be filled, and they dare to create something for which a market may not yet exist. Yet executing on that disruption requires the learning and growth of your people. You know your people want to make progress, but don't always know how. Sometimes they don't know where to start. Once they start, they can struggle to maintain momentum, and once they've made considerable progress, they may tire of what they've been doing and need to do something new.</p>
+          <p class="text-[#4d4d4d]">In this keynote, you'll learn how to use the S Curve of Learning&trade; Framework to identify where you and each member of your team is in their growth and how to help them navigate their growth––from the launch point to sweet spot to mastery. The fundamental unit of growth is the individual. Companies don't disrupt, people do. </p>
+          <a class="group flex items-center font-bold text-[#017381] hover:text-[#143b3f] cursor-pointer" data-js="fireContactModal">
+            <span class="mr-1 group-hover:underline">Inquire Now</span>
+            <span class="material-icons-round mr">arrow_forward</span>
+          </a>
+        </div>
       </div>
     </div>
     <!-- Build an A Team -->
-    <div class="flex flex-col md:flex-row border-b border-[#BFBFBF] pb-8 lg:border-none lg:pb-0">
+    <div class="flex flex-col md:flex-row border-b border-[#d4d4d4] pb-8">
       <div class="mb-8 md:mb-0 md:mr-8 flex-shrink-0">
         <div class="relative">
           <img class="w-full md:w-[320px] lg:w-[500px]" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/services/video-build-an-a-team.png') ?>" alt="" width="500" height="274" />
         </div>
       </div>
       <div class="space-y-4">
-        <h4 class="text-xl font-bold">Build an A Team</h4>
-        <p class="text-[#4d4d4d]">We all want opportunities to learn, experiment, and grow in our jobs. The best bosses know this and how to make it happen through thoughtful role design and challenge. The result is a team that learns how to thrive, no matter what the industry throws at them.</p>
+        <h4 class="text-xl font-bold">Smart Growth – The Key to Growing Your Company</h4>
+        <p class="text-[#4d4d4d]">The most important funding mechanism for your company's growth is the learning and growth of your people. But somehow jobs muffle people's innate desire to learn––they find themselves bored, stagnant, or wanting more at work. In this keynote, you will learn about the S Curve of Learning&trade;, a model for smart growth. We will answer questions like, why, despite the desire to grow, can it be so difficult to start? What does it take to gain and maintain momentum once people start? And finally, why do people outgrow things so quickly? The answers to these questions prepare you to understand the growth process and identify where each person, including yourself, is in his/her growth. When you know where people are on their S Curves, you can help and support them as they move forward. Grow yourself to grow your people to grow your company. That's smart growth.</p>
         <a class="group flex items-center font-bold text-[#017381] hover:text-[#143b3f] cursor-pointer" data-js="fireContactModal">
           <span class="mr-1 group-hover:underline">Inquire Now</span>
           <span class="material-icons-round mr">arrow_forward</span>
@@ -41,15 +45,16 @@
       </div>
     </div>
     <!-- Smart Growth -->
-    <div class="flex flex-col md:flex-row border-b border-[#BFBFBF] pb-8 lg:border-none lg:pb-0">
+    <div class="flex flex-col md:flex-row border-b border-[#d4d4d4] pb-8">
       <div class="mb-8 md:mb-0 md:mr-8 flex-shrink-0">
         <div class="relative">
           <img class="w-full md:w-[320px] lg:w-[500px]" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/services/video-smart-growth.png') ?>" alt="" width="500" height="274" />
         </div>
       </div>
       <div class="space-y-4">
-        <h4 class="text-xl font-bold">Smart Growth</h4>
-        <p class="text-[#4d4d4d]">Faucibus eget quam interdum pellentesque pretium nisl tristique eget urna aliquam ut lacus mauris maecenas tellus ornare et nec vitae consequat neque maecenas viverra quis morbi tortor vel sollicitudin ornare scelerisque egestas at aliquet viverra faucibus proin nunc euismod est</p>
+        <h4 class="text-xl font-bold">How to Build a High Performing Team</h4>
+        <p class="text-[#4d4d4d]">The secret to building a high-performing team is to have a plan for developing each employee no matter where they are on their personal learning curves. In this keynote, you will learn about the S Curve of Learning&trade; Framework and to identify where you and each member of your team is in their growth and then help them navigate their growth––from the launch point to the sweet spot to mastery.</p>
+        <p class="text-[#4d4d4d]">Your team is a collection of S Curves. You build a team that can carry the high load of growth by building a portfolio of individual S Curves that matches your organization's current needs. When you help each person navigate the entire growth cycle––help them learn, leap and repeat––it is impossible not to gain an organizational advantage. Don't build just one A Team, build vast networks of A Teams across companies, careers, and lives.</p>
         <a class="group flex items-center font-bold text-[#017381] hover:text-[#143b3f] cursor-pointer" data-js="fireContactModal">
           <span class="mr-1 group-hover:underline">Inquire Now</span>
           <span class="material-icons-round mr">arrow_forward</span>
@@ -64,8 +69,8 @@
         </div>
       </div>
       <div class="space-y-4">
-        <h4 class="text-xl font-bold">The Next Step: Lessons on embracing change and continuous improvement</h4>
-        <p class="text-[#4d4d4d]">Learn to leverage disruption and build momentum along your S Curveof Learning by leveraging the Seven Accelerants of Growth. Feel confident and move toward continuous improvement rather thansuccumbing to the chaos of change. Whatever the challenges, it is the individual that is the fundamental unit of growth.</p>
+        <h4 class="text-xl font-bold">Take Charge of Your Career, Your Organization, and Your Life</h4>
+        <p class="text-[#4d4d4d]">We are living in an era of accelerating disruption––no one is immune. But how you deal with disruption defines you. In this keynote, you will learn about the Seven-point Framework of Personal Disruption, a proven algorithm for accelerating your growth. Whether you are early-career, mid-career, or a seasoned professional, deliberate self-innovation will help you build momentum along the various growth curves. If you want to be successful in unexpected ways, follow your own disruptive path. Dare to innovate. Do something astonishing. Disrupt yourself!</p>
         <a class="group flex items-center font-bold text-[#017381] hover:text-[#143b3f] cursor-pointer" data-js="fireContactModal">
           <span class="mr-1 group-hover:underline">Inquire Now</span>
           <span class="material-icons-round mr">arrow_forward</span>
