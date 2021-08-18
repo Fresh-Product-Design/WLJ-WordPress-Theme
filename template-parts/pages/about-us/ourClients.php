38087609990191c1d1326b263abd5043039c8234
Zach Case <zach@sherpadigitalmedia.com>
2021-08-18 15:14:00 -0600

    Update about us logos

--- template-parts/pages/about-us/ourClients.php
+++ template-parts/pages/about-us/ourClients.php
@@ -1,14 +1,25 @@
 <div id="clients" class="content-container py-24">
-  <?php get_template_part( 'template-parts/components/headers/basic', null, array("label" => "Our Clients", "mb" => "mb-2")); ?>
+  <?php get_template_part( 'template-parts/components/headers/basic', null, array("label" => "Our Clients", "mb" => "mb-0")); ?>
   <div class="flex items-center justify-evenly flex-wrap">
-    <img class="w-[133px] lg:w-[175px] pt-8 sm:pt-4 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/bbq-guys.png') ?>" alt="" width="133" height="53" />
-    <img class="w-[133px] lg:w-[175px] pt-8 sm:pt-4 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/weave.png') ?>" alt="" width="133" height="53" />
-    <img class="w-[133px] lg:w-[175px] pt-8 sm:pt-4 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/kraft-heinz.png') ?>" alt="" width="133" height="53" />
-    <img class="w-[133px] lg:w-[175px] pt-8 sm:pt-4 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/WGU.png') ?>" alt="" width="133" height="53" />
-    <img class="w-[133px] lg:w-[175px] pt-8 sm:pt-4 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/beautybio.png') ?>" alt="" width="133" height="53" />
-    <img class="w-[133px] lg:w-[175px] pt-8 sm:pt-4 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/BYU.png') ?>" alt="" width="133" height="53" />
-    <img class="w-[133px] lg:w-[175px] pt-8 sm:pt-4 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/cisco.png') ?>" alt="" width="133" height="53" />
-    <img class="w-[133px] lg:w-[175px] pt-8 sm:pt-4 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/unified.png') ?>" alt="" width="133" height="53" />
-    <img class="w-[133px] lg:w-[175px] pt-8 sm:pt-4 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/worldbankgroup.png') ?>" alt="" width="133" height="53" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/brand-velocity-partners.png') ?>" alt="" width="133" height="89" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/broward-college.png') ?>" alt="" width="133" height="34" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/YPO.png') ?>" alt="" width="133" height="50" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/morgan-stanley.png') ?>" alt="" width="133" height="91" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/wobi.png') ?>" alt="" width="133" height="36" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/HBPCorpLearning.png') ?>" alt="" width="133" height="17" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/WBECS.png') ?>" alt="" width="133" height="21" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/hubspot.png') ?>" alt="" width="133" height="49" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/estee-lauder.png') ?>" alt="" width="133" height="49" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/BYU.png') ?>" alt="" width="133" height="75" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/gartner.png') ?>" alt="" width="133" height="31" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/citizens-bank.png') ?>" alt="" width="133" height="20" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/stoxx.png') ?>" alt="" width="133" height="30" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/dropbox.png') ?>" alt="" width="133" height="30" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/tricor.png') ?>" alt="" width="133" height="42" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/nga.png') ?>" alt="" width="133" height="43" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/sage-hospitality.png') ?>" alt="" width="133" height="59" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/original-footwear.png') ?>" alt="" width="133" height="47" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/wd-40.png') ?>" alt="" width="133" height="53" />
+    <img class="w-[133px] lg:w-[175px] pt-12 sm:pt-8 px-6 box-content" src="<?php esc_attr_e(get_template_directory_uri() . '/src/assets/images/clients/utah-commerce.png') ?>" alt="" width="133" height="43" />
   </div>
 </div>
--- template-parts/pages/home/SCurveInsight.php
+++ template-parts/pages/home/SCurveInsight.php
@@ -5,18 +5,18 @@
     <?php get_template_part( 'template-parts/components/headers/basic', null, array("label" => "S Curve Insight&trade; Platform", "mb" => "mb-2")); ?>
     <p class="max-w-[908px] text-lg text-[#4d4d4d] mx-auto mb-16">The S Curve Insight Platform is inspired by the world's leading innovation and management thinkers and is used by FORTUNE 500 companies who understand that they fund their company's growth via the learning and growth of their people.</p>
     <div class="w-full mb-16 overflow-auto">
-      <div class="inline-flex space-x-2 text-left w-[843px]">
-        <div class="bg-white bg-scurve-start bg-bottom bg-no-repeat w-[275px] h-[453px] p-4 rounded">
+      <div class="inline-flex flex-col lg:flex-row text-left space-y-4 lg:space-y-0 lg:space-x-2 lg:w-[843px]">
+        <div class="bg-white lg:bg-scurve-start bg-bottom bg-no-repeat w-full lg:w-[275px] lg:h-[453px] p-4 rounded">
           <p class="text-sm text-[#808080] mb-2">Step 1.</p>
           <h4 class="text-xl font-bold mb-4">Take the Assessment</h4>
           <p class="text-sm text-[#4d4d4d]">See where each individual is, including you, on their growth journey.</p>
         </div>
-        <div class="bg-white bg-scurve-mid bg-bottom bg-no-repeat w-[275px] h-[453px] p-4 rounded">
+        <div class="bg-white lg:bg-scurve-mid bg-bottom bg-no-repeat w-full lg:w-[275px] lg:h-[453px] p-4 rounded">
           <p class="text-sm text-[#808080] mb-2">Step 2.</p>
           <h4 class="text-xl font-bold mb-4">Accelerate Growth</h4>
           <p class="text-sm text-[#4d4d4d]">Gain actionable insight into how you can grow faster.</p>
         </div>
-        <div class="bg-white bg-scurve-end bg-bottom bg-no-repeat w-[275px] h-[453px] p-4 rounded">
+        <div class="bg-white lg:bg-scurve-end bg-bottom bg-no-repeat w-full lg:w-[275px] lg:h-[453px] p-4 rounded">
           <p class="text-sm text-[#808080] mb-2">Step 3.</p>
           <h4 class="text-xl font-bold mb-4">Achieve Potential</h4>
           <p class="text-sm text-[#4d4d4d]">Build momentum by coaching to the curve.</p>
