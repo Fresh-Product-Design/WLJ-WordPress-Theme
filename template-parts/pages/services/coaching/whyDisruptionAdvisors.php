7d4136754301a291ffbeeb4166bee9f6a2ec7cfb
Zach Case <zach@sherpadigitalmedia.com>
2021-08-17 18:05:25 -0600

    Remove paragraph

--- template-parts/pages/services/coaching/whyDisruptionAdvisors.php
+++ template-parts/pages/services/coaching/whyDisruptionAdvisors.php
@@ -1,6 +1,5 @@
 <div class="content-container py-24">
-  <?php get_template_part( 'template-parts/components/headers/basic', null, array("label" => "Why Disruption Advisors?", "mb" => "mb-2")); ?>
-  <p class="max-w-[903px] text-[#4d4d4d] text-lg text-center mx-auto mb-16">We'll work together to create and implement a solution so you can effectively lead your team through change.</p>
+  <?php get_template_part( 'template-parts/components/headers/basic', null, array("label" => "Why Disruption Advisors?", "mb" => "mb-16")); ?>
   <!-- Why Disruption Advisors Grid -->
   <?php get_template_part( 'template-parts/pages/services/coaching/comparisonGrid' ); ?>
   <!-- What is the S Curve Insight Platform? -->
